1db0f8649ea787b3430666e7024404e3c08e8a01
Zoksss <zorantodorovic80@gmail.com>
2023-05-24 20:47:56 +0200

    roblox portoflio update

---
+++ js/buildJs.js
@@ -0,0 +1,22 @@
+const imageArray = [
+    [
+        "./images/map0/map0_1.png",
+        "./images/map0/map0_2.png",
+        "./images/map0/map0_3.png",
+        "./images/map0/map0_4.png",
+        "./images/map0/map0_5.png",
+        "./images/map0/map0_6.png",
+        "./images/map0/map0_7.png",
+        "./images/map0/map0_8.png",
+        "./images/map0/map0_9.png",
+        "./images/map0/map0_10.png",
+        "./images/map0/map0_11.png",
+        "./images/map0/map0_12.png",
+    ],
+]
+
+const loadBuildImages = (num) => {
+    let currArray = imageArray[num];
+    console.log(currArray);
+    
+}
